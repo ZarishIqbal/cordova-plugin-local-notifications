3c9e084f7151b6cd5cdd3aa039d8c0c8d05ddcc0
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-05 11:06:01 +0300

    Update local-notification.js

--- www/local-notification.js
+++ www/local-notification.js
@@ -18,3 +18,70 @@
  * Please see the License for the specific language governing rights and
  * limitations under the License.
  */
+
+var exec    = require('cordova/exec'),
+    channel = require('cordova/channel');
+
+// Defaults
+exports._defaults = {
+    actions       : [],
+    attachments   : [],
+    autoClear     : true,
+    badge         : null,
+    channel       : null,
+    clock         : true,
+    color         : null,
+    data          : null,
+    defaults      : 0,
+    foreground    : null,
+    group         : null,
+    groupSummary  : false,
+    icon          : null,
+    iconType      : null,
+    id            : 0,
+    launch        : true,
+    led           : true,
+    lockscreen    : true,
+    mediaSession  : null,
+    number        : 0,
+    priority      : 0,
+    progressBar   : false,
+    silent        : false,
+    smallIcon     : 'res://icon',
+    sound         : true,
+    sticky        : false,
+    summary       : null,
+    text          : '',
+    timeoutAfter  : false,
+    title         : '',
+    trigger       : { type : 'calendar' },
+    vibrate       : false,
+    wakeup        : true
+};
+
+// Event listener
+exports._listener = {};
+
+/**
+ * Check permission to show notifications.
+ *
+ * @param [ Function ] callback The function to be exec as the callback.
+ * @param [ Object ]   scope    The callback function's scope.
+ *
+ * @return [ Void ]
+ */
+exports.hasPermission = function (callback, scope) {
+    this._exec('check', null, callback, scope);
+};
+
+/**
+ * Request permission to show notifications.
+ *
+ * @param [ Function ] callback The function to be exec as the callback.
+ * @param [ Object ]   scope    The callback function's scope.
+ *
+ * @return [ Void ]
+ */
+exports.requestPermission = function (callback, scope) {
+    this._exec('request', null, callback, scope);
+};
