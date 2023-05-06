6e903e13e44731c35828a27e6727f620ee9a5f55
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-06 14:56:42 +0300

    Update LocalNotification.java

--- src/android/LocalNotification.java
+++ src/android/LocalNotification.java
@@ -127,3 +127,153 @@ public class LocalNotification extends CordovaPlugin {
             launch(command);
             return true;
         }
+
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                if (action.equals("ready")) {
+                    deviceready();
+                } else
+                if (action.equals("check")) {
+                    check(command);
+                } else
+                if (action.equals("request")) {
+                    request(command);
+                } else
+                if (action.equals("actions")) {
+                    actions(args, command);
+                } else
+                if (action.equals("schedule")) {
+                    schedule(args, command);
+                } else
+                if (action.equals("update")) {
+                    update(args, command);
+                } else
+                if (action.equals("cancel")) {
+                    cancel(args, command);
+                } else
+                if (action.equals("cancelAll")) {
+                    cancelAll(command);
+                } else
+                if (action.equals("clear")) {
+                    clear(args, command);
+                } else
+                if (action.equals("clearAll")) {
+                    clearAll(command);
+                } else
+                if (action.equals("type")) {
+                    type(args, command);
+                } else
+                if (action.equals("ids")) {
+                    ids(args, command);
+                } else
+                if (action.equals("notification")) {
+                    notification(args, command);
+                } else
+                if (action.equals("notifications")) {
+                    notifications(args, command);
+                }
+            }
+        });
+
+        return true;
+    }
+
+    /**
+     * Set launchDetails object.
+     *
+     * @param command The callback context used when calling back into
+     *                JavaScript.
+     */
+    @SuppressLint("DefaultLocale")
+    private void launch(CallbackContext command) {
+        if (launchDetails == null)
+            return;
+
+        JSONObject details = new JSONObject();
+
+        try {
+            details.put("id", launchDetails.first);
+            details.put("action", launchDetails.second);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        command.success(details);
+
+        launchDetails = null;
+    }
+
+    /**
+     * Ask if user has enabled permission for local notifications.
+     *
+     * @param command The callback context used when calling back into
+     *                JavaScript.
+     */
+    private void check (CallbackContext command) {
+        boolean allowed = getNotMgr().hasPermission();
+        success(command, allowed);
+    }
+
+    /**
+     * Request permission for local notifications.
+     *
+     * @param command The callback context used when calling back into
+     *                JavaScript.
+     */
+    private void request (CallbackContext command) {
+        check(command);
+    }
+
+    /**
+     * Register action group.
+     *
+     * @param args    The exec() arguments in JSON form.
+     * @param command The callback context used when calling back into
+     *                JavaScript.
+     */
+    private void actions (JSONArray args, CallbackContext command) {
+        int task        = args.optInt(0);
+        String id       = args.optString(1);
+        JSONArray list  = args.optJSONArray(2);
+        Context context = cordova.getActivity();
+
+        switch (task) {
+            case 0:
+                ActionGroup group = ActionGroup.parse(context, id, list);
+                ActionGroup.register(group);
+                command.success();
+                break;
+            case 1:
+                ActionGroup.unregister(id);
+                command.success();
+                break;
+            case 2:
+                boolean found = ActionGroup.isRegistered(id);
+                success(command, found);
+                break;
+        }
+    }
+
+    /**
+     * Schedule multiple local notifications.
+     *
+     * @param toasts  The notifications to schedule.
+     * @param command The callback context used when calling back into
+     *                JavaScript.
+     */
+    private void schedule (JSONArray toasts, CallbackContext command) {
+        Manager mgr = getNotMgr();
+
+        for (int i = 0; i < toasts.length(); i++) {
+            JSONObject dict    = toasts.optJSONObject(i);
+            Options options    = new Options(dict);
+            Request request    = new Request(options);
+            Notification toast = mgr.schedule(request, TriggerReceiver.class);
+
+            if (toast != null) {
+                fireEvent("add", toast);
+            }
+        }
+
+        check(command);
+    }
